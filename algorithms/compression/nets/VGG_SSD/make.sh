9f9f31cb402f4f1cb031b670e9e953695945c25f
BowmanChow <bowmanchow@gmail.com>
2023-12-11 11:24:39 +0800

    modify python version in make.sh

--- algorithms/compression/nets/VGG_SSD/make.sh
+++ algorithms/compression/nets/VGG_SSD/make.sh
@@ -6,6 +6,7 @@ CUDA_PATH=/usr/local/cuda/
 python build.py build_ext --inplace
 # if you use anaconda3 maybe you need add this
 # change code like https://github.com/rbgirshick/py-faster-rcnn/issues/706
-mv nms/cpu_nms.cpython-36m-x86_64-linux-gnu.so nms/cpu_nms.so
-mv nms/gpu_nms.cpython-36m-x86_64-linux-gnu.so nms/gpu_nms.so
+python_version=$(python -c "import platform; v=platform.python_version_tuple(); print(v[0]+v[1])")
+mv nms/cpu_nms.cpython-${python_version}-x86_64-linux-gnu.so nms/cpu_nms.so
+mv nms/gpu_nms.cpython-${python_version}-x86_64-linux-gnu.so nms/gpu_nms.so
 cd ..
