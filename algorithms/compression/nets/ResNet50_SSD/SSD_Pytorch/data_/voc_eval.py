a6004d9c75c0d1675237c3a4bb6a21f27258f294
BowmanChow <bowmanchow@gmail.com>
2023-12-11 13:06:07 +0800

    np.bool -> bool

--- algorithms/compression/nets/ResNet50_SSD/SSD_Pytorch/data_/voc_eval.py
+++ algorithms/compression/nets/ResNet50_SSD/SSD_Pytorch/data_/voc_eval.py
@@ -123,7 +123,7 @@ def voc_eval(detpath, annopath, imagesetfile, classname, cachedir, ovthresh=0.5,
     for imagename in imagenames:
         R = [obj for obj in recs[imagename] if obj['name'] == classname]
         bbox = np.array([x['bbox'] for x in R])
-        difficult = np.array([x['difficult'] for x in R]).astype(np.bool)
+        difficult = np.array([x['difficult'] for x in R]).astype(bool)
         det = [False] * len(R)
         npos = npos + sum(~difficult)
         class_recs[imagename] = {'bbox': bbox, 'difficult': difficult, 'det': det}
